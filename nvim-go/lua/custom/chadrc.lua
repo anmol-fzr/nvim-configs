a5225f3b5aed861ccdd698cf27551a44f517fc2e
Anmol <ainsa2279@gmail.com>
2023-12-26 20:46:08 +0530

    add : go and nextjs

---
+++ nvim-go/lua/custom/chadrc.lua
@@ -0,0 +1,5 @@
+---@type ChadrcConfig
+local M = {}
+M.ui = { theme = 'catppuccin',transparency=true }
+M.plugins = "custom.plugins"
+return M
---
+++ nvim-go/lua/custom/plugins.lua
@@ -0,0 +1,29 @@
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+      require("core.utils").load_mappings("gopher")
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
+}
+return plugins
